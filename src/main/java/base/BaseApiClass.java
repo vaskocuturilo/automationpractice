255bea0c3ba9e65dd3167cce5e30b09f0733cd11
Vasko <vaskocuturilo@rambler.ru`>
2019-08-05 11:03:36 +0300

    Feat : BaseApiClass : Added base class for testing api. Commit at 02/08/2019

---
+++ src/main/java/base/BaseApiClass.java
@@ -0,0 +1,16 @@
+package base;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeMethod;
+
+import static utils.PropertiesReader.loadProperty;
+
+public class BaseApiClass {
+
+
+    @BeforeMethod(alwaysRun = true)
+    public void beforeTest() {
+
+        RestAssured.baseURI = loadProperty("");
+    }
+}
