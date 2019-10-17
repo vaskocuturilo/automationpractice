2021a967241a86147f07acf8657840cd726a4554
vaskocuturilo@rambler.ru <wolfstonik54321>
2019-10-17 13:38:55 +0300

    Feat : CountryCode: Added JavaDoc.
    Commit at 17/10/2019

--- src/main/java/api/schema/CountryCode.java
+++ src/main/java/api/schema/CountryCode.java
@@ -4,7 +4,11 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import javax.annotation.Generated;
 
+/**
+ * The type Country code.
+ */
 @Generated("com.robohorse.robopojogenerator")
+
 public class CountryCode{
 
 	@JsonProperty("area")
@@ -79,98 +83,218 @@ public class CountryCode{
 	@JsonProperty("currencies")
 	private List<CurrenciesItem> currencies;
 
+	/**
+	 * Get area int.
+	 *
+	 * @return the int
+	 */
 	public int getArea(){
 		return area;
 	}
 
+	/**
+	 * Get native name string.
+	 *
+	 * @return the string
+	 */
 	public String getNativeName(){
 		return nativeName;
 	}
 
+	/**
+	 * Get capital string.
+	 *
+	 * @return the string
+	 */
 	public String getCapital(){
 		return capital;
 	}
 
+	/**
+	 * Get demonym string.
+	 *
+	 * @return the string
+	 */
 	public String getDemonym(){
 		return demonym;
 	}
 
+	/**
+	 * Get flag string.
+	 *
+	 * @return the string
+	 */
 	public String getFlag(){
 		return flag;
 	}
 
+	/**
+	 * Get alpha 2 code string.
+	 *
+	 * @return the string
+	 */
 	public String getAlpha2Code(){
 		return alpha2Code;
 	}
 
+	/**
+	 * Get languages list.
+	 *
+	 * @return the list
+	 */
 	public List<LanguagesItem> getLanguages(){
 		return languages;
 	}
 
+	/**
+	 * Get borders list.
+	 *
+	 * @return the list
+	 */
 	public List<String> getBorders(){
 		return borders;
 	}
 
+	/**
+	 * Get subregion string.
+	 *
+	 * @return the string
+	 */
 	public String getSubregion(){
 		return subregion;
 	}
 
+	/**
+	 * Get calling codes list.
+	 *
+	 * @return the list
+	 */
 	public List<String> getCallingCodes(){
 		return callingCodes;
 	}
 
+	/**
+	 * Get regional blocs list.
+	 *
+	 * @return the list
+	 */
 	public List<RegionalBlocsItem> getRegionalBlocs(){
 		return regionalBlocs;
 	}
 
+	/**
+	 * Get gini double.
+	 *
+	 * @return the double
+	 */
 	public double getGini(){
 		return gini;
 	}
 
+	/**
+	 * Get population int.
+	 *
+	 * @return the int
+	 */
 	public int getPopulation(){
 		return population;
 	}
 
+	/**
+	 * Get numeric code string.
+	 *
+	 * @return the string
+	 */
 	public String getNumericCode(){
 		return numericCode;
 	}
 
+	/**
+	 * Get alpha 3 code string.
+	 *
+	 * @return the string
+	 */
 	public String getAlpha3Code(){
 		return alpha3Code;
 	}
 
+	/**
+	 * Get top level domain list.
+	 *
+	 * @return the list
+	 */
 	public List<String> getTopLevelDomain(){
 		return topLevelDomain;
 	}
 
+	/**
+	 * Get timezones list.
+	 *
+	 * @return the list
+	 */
 	public List<String> getTimezones(){
 		return timezones;
 	}
 
+	/**
+	 * Get cioc string.
+	 *
+	 * @return the string
+	 */
 	public String getCioc(){
 		return cioc;
 	}
 
+	/**
+	 * Get translations translations.
+	 *
+	 * @return the translations
+	 */
 	public Translations getTranslations(){
 		return translations;
 	}
 
+	/**
+	 * Get name string.
+	 *
+	 * @return the string
+	 */
 	public String getName(){
 		return name;
 	}
 
+	/**
+	 * Get alt spellings list.
+	 *
+	 * @return the list
+	 */
 	public List<String> getAltSpellings(){
 		return altSpellings;
 	}
 
+	/**
+	 * Get region string.
+	 *
+	 * @return the string
+	 */
 	public String getRegion(){
 		return region;
 	}
 
+	/**
+	 * Get latlng list.
+	 *
+	 * @return the list
+	 */
 	public List<Integer> getLatlng(){
 		return latlng;
 	}
 
+	/**
+	 * Get currencies list.
+	 *
+	 * @return the list
+	 */
 	public List<CurrenciesItem> getCurrencies(){
 		return currencies;
 	}
